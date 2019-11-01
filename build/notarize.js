90991142ba801b340be7e2f3ab4b5ec8efa8dc1a
Swen van Zanten <info@hdsign.nl>
2019-11-01 18:44:25 +0100

    change notarize approach

--- build/notarize.js
+++ build/notarize.js
@@ -3,21 +3,19 @@ const {notarize} = require('electron-notarize')
 
 exports.default = async function notarizing (context) {
   const {electronPlatformName, appOutDir} = context
-  if (!process.env.NOTARIZE) {
-    return
-  }
-
   if (electronPlatformName !== 'darwin') {
     return
   }
 
   const appName = context.packager.appInfo.productFilename
 
-  return await notarize({
-    appBundleId: 'org.verge.my-vergies',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS,
-    ascProvider: process.env.APPLE_ASC_PROVIDER
-  })
+  if (!process.env.NOTARIZE) {
+    return await notarize({
+      appBundleId: 'org.verge.my-vergies',
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_ID_PASS,
+      ascProvider: process.env.APPLE_ASC_PROVIDER
+    })
+  }
 }
