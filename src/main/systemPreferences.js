ce6f3924334baba19466e2dad50b60ad016f65c5
Swen van Zanten <info@hdsign.nl>
2019-11-02 13:21:05 +0100

    Check if we're working on darwin for subscribe for theme change

--- src/main/systemPreferences.js
+++ src/main/systemPreferences.js
@@ -3,10 +3,12 @@ import { systemPreferences } from 'electron'
 /**
  * Listen for macOS appearance changes.
  */
-systemPreferences.subscribeNotification(
-  'AppleInterfaceThemeChangedNotification',
-  function systemThemeChanged () {
-    console.log('System appearance is DarkMode: ' + systemPreferences.isDarkMode())
-    // updateMyAppTheme(systemPreferences.isDarkMode())
-  }
-)
+if (process.platform === 'darwin') {
+  systemPreferences.subscribeNotification(
+    'AppleInterfaceThemeChangedNotification',
+    function systemThemeChanged () {
+      console.log('System appearance is DarkMode: ' + systemPreferences.isDarkMode())
+      // updateMyAppTheme(systemPreferences.isDarkMode())
+    }
+  )
+}
