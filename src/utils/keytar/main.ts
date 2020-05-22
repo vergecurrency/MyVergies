c899aca61c7461c5c50576b415fd3e6e44c00cf3
Swen van Zanten <info@hdsign.nl>
2020-05-22 19:21:28 +0200

    fix async keytar call

--- src/utils/keytar/main.ts
+++ src/utils/keytar/main.ts
@@ -1,14 +1,14 @@
 import keytar from 'keytar'
 import electron from 'electron'
 
-electron.ipcMain.on('get-password', (event, service: string, wallet: string) => {
-  event.returnValue = keytar.getPassword(service, wallet)
+electron.ipcMain.on('get-password', async (event, service: string, wallet: string) => {
+  event.returnValue = await keytar.getPassword(service, wallet)
 })
 
-electron.ipcMain.on('set-password', (event, service: string, wallet: string, credentials: string) => {
-  event.returnValue = keytar.setPassword(service, wallet, credentials)
+electron.ipcMain.on('set-password', async (event, service: string, wallet: string, credentials: string) => {
+  event.returnValue = await keytar.setPassword(service, wallet, credentials)
 })
 
-electron.ipcMain.on('delete-password', (event, service: string, wallet: string) => {
-  event.returnValue = keytar.deletePassword(service, wallet)
+electron.ipcMain.on('delete-password', async (event, service: string, wallet: string) => {
+  event.returnValue = await keytar.deletePassword(service, wallet)
 })
