d552ea15cb208fa8317abce637b058819ca7aa7d
marpme <marpme@users.noreply.github.com>
2020-05-24 20:38:45 +0200

    Setup proxy compatbility

--- src/background.ts
+++ src/background.ts
@@ -2,6 +2,7 @@
 
 import { app, protocol, nativeTheme, BrowserWindow, Menu } from 'electron'
 import { autoUpdater } from 'electron-updater'
+import * as Tor from '@deadcanaries/granax'
 import { createProtocol, installVueDevtools } from 'vue-cli-plugin-electron-builder/lib'
 import { generateMenuTemplate, dockTemplate } from '@/toolbar/menu'
 import logger from 'electron-log'
@@ -14,7 +15,7 @@ logger.transports.file.level = 'debug'
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win: BrowserWindow | null
-
+const TOR_SOCKS_PORT = 9999
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([{ scheme: 'app', privileges: { secure: true, standard: true } }])
 
@@ -52,17 +53,24 @@ function createWindow () {
     }
   })
 
-  mainWindowState.manage(win)
+  win.webContents.session.setProxy({
+    proxyRules: `socks5://127.0.0.1:${TOR_SOCKS_PORT}`,
+    proxyBypassRules: '<local>, 192.168.1.1/16, fefe:13::abc/33'
+  }).then(() => {
+    console.log('Socks proxy set')
 
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    mainWindowState.manage(win!)
+
+    if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
-  } else {
-    createProtocol('app')
-    // Load the index.html when not in development
-    win.loadURL('app://./index.html')
-  }
+      win!.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
+      if (!process.env.IS_TEST) win!.webContents.openDevTools()
+    } else {
+      createProtocol('app')
+      // Load the index.html when not in development
+      win!.loadURL('app://./index.html')
+    }
+  })
 
   if (isMacOSEnvironment()) {
     let forceQuit = false
@@ -108,24 +116,45 @@ app.on('activate', () => {
   }
 })
 
+const startUpTorOnPort = (port: number) => {
+  const tor = Tor()
+  return new Promise((resolve, reject) => {
+    tor.on('ready', async () => {
+      tor.setConfig('SocksPort', `${port}`, () => {
+        tor.getConfig('SocksPort', (_: any, result: any) => {
+          resolve(result)
+        })
+      })
+    })
+
+    tor.on('error', (err: Error) => {
+      reject(err)
+    })
+  })
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  if (isDevelopmentEnvironment() && !process.env.IS_TEST) {
+  startUpTorOnPort(TOR_SOCKS_PORT).then(async port => {
+    console.log(`TorSocks listening on ${port}!`)
+
+    if (isDevelopmentEnvironment() && !process.env.IS_TEST) {
     // Install Vue Devtools
     // Devtools extensions are broken in Electron 6.0.0 and greater
     // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
     // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
     // If you are not using Windows 10 dark mode, you may uncomment these lines
     // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
-    try {
-      await installVueDevtools()
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+      try {
+        await installVueDevtools()
+      } catch (e) {
+        console.error('Vue Devtools failed to install:', e.toString())
+      }
     }
-  }
-  createWindow()
+    createWindow()
+  })
 })
 
 // Exit cleanly on request from parent process in development mode.
