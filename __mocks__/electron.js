e517e69408f22dc2e64393eb6a9baf5690cfa824
Swen van Zanten <info@hdsign.nl>
2020-05-25 22:53:41 +0200

    fix tests

--- __mocks__/electron.js
+++ __mocks__/electron.js
@@ -1,15 +1,16 @@
 'use strict'
 
-
 module.exports = {
-  MenuItem: jest.fn(function(values) {
+  MenuItem: jest.fn(function (values) {
     this.values = values
   }),
   Menu: jest.fn(() => ({
     append: jest.fn()
   })),
   app: {
-    name: "MyVergies"
+    name: 'MyVergies',
+    getVersion () {
+      return '0.0.1'
+    }
   }
-};
-
+}
