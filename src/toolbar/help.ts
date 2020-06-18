1b6dd69d471b7bc46b79a03866d1dbf2bc601565
Swen van Zanten <info@hdsign.nl>
2020-06-18 10:41:35 +0200

    fix menu items

--- src/toolbar/help.ts
+++ src/toolbar/help.ts
@@ -1,11 +1,34 @@
 import { MenuItem, Menu } from 'electron'
 
-const SubMenuItems: MenuItem[] = [
+let SubMenuItems: MenuItem[] = [
   new MenuItem({
-    role: 'minimize'
+    label: 'Learn how to create a new wallet'
   }),
   new MenuItem({
-    role: 'close'
+    label: 'Learn how to restore an existing wallet'
+  }),
+  new MenuItem({
+    label: 'Learn how to send XVG'
+  }),
+  new MenuItem({
+    label: 'Learn how to receive XVG'
+  }),
+  new MenuItem({
+    type: 'separator'
+  }),
+  new MenuItem({
+    label: 'Visit our GitHub',
+    click: () => {
+      require('electron').shell.openExternal(
+        'https://github.com/vergecurrency'
+      )
+    }
+  }),
+  new MenuItem({
+    label: 'Visit vergecurrency.com',
+    click: () => {
+      require('electron').shell.openExternal('https://vergecurrency.com')
+    }
   })
 ]
 
--- src/toolbar/window.ts
+++ src/toolbar/window.ts
@@ -3,33 +3,10 @@ import { isWinOSEnvironment } from '@/utils'
 
 let SubMenuItems: MenuItem[] = [
   new MenuItem({
-    label: 'Learn how to create a new wallet'
+    role: 'minimize'
   }),
   new MenuItem({
-    label: 'Learn how to restore an existing wallet'
-  }),
-  new MenuItem({
-    label: 'Learn how to send XVG'
-  }),
-  new MenuItem({
-    label: 'Learn how to receive XVG'
-  }),
-  new MenuItem({
-    type: 'separator'
-  }),
-  new MenuItem({
-    label: 'Visit our GitHub',
-    click: () => {
-      require('electron').shell.openExternal(
-        'https://github.com/vergecurrency'
-      )
-    }
-  }),
-  new MenuItem({
-    label: 'Visit vergecurrency.com',
-    click: () => {
-      require('electron').shell.openExternal('https://vergecurrency.com')
-    }
+    role: 'close'
   })
 ]
 
